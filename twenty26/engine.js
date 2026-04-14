69f83c9b625ab21c4baf33d00e1a1c7d936c19c0
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-04-15 08:13:49 +1200

    Update engine.js

--- twenty26/engine.js
+++ twenty26/engine.js
@@ -6,7 +6,7 @@ console.log("Engine loaded");
 
 // --- BOOTSTRAP FETCH ---------------------------------------------------------
 
-fetch("/bootstrap", { method: "POST" })
+fetch(WW_CONFIG.api.bootstrap, { method: "POST" })
   .then(r => r.json())
   .then(data => {
     start(data);
