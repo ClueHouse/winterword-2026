71458fa5c6a32ffe614f1a1788b16683723cf84d
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-04-15 11:12:37 +1200

    Delete twenty26/config.js

--- twenty26/config.js
+++
@@ -1,9 +0,0 @@
-export const WW_CONFIG = {
-  version: "2026.1",
-  enginePath: "/twenty26/engine.js",
-  api: {
-    bootstrap: "/bootstrap",
-    asset: "/api/asset",
-    image: "/api/image"
-  }
-};
