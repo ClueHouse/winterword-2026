f2f3391cfdb3eda328078f0afd3a7d2392d97141
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-04-14 15:17:43 +1200

    Create config.js

---
+++ twenty26/config.js
@@ -0,0 +1,9 @@
+export const WW_CONFIG = {
+  version: "2026.1",
+  enginePath: "/js/twenty26/engine.js",
+  api: {
+    bootstrap: "/api/bootstrap",
+    asset: "/api/asset",
+    image: "/api/image"
+  }
+};
