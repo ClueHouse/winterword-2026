bdd10f89b618951a461fd6050d1ef01e7d69a37c
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-04-16 10:45:31 +1200

    Update config.js

--- public/twenty26/config.js
+++ public/twenty26/config.js
@@ -2,8 +2,8 @@ export const WW_CONFIG = {
   version: "2026.1",
   enginePath: "/twenty26/engine.js",
   api: {
-    bootstrap: "/bootstrap",
-    asset: "/api/asset",
-    image: "/api/image"
+    bootstrap: "/assets/winterword/api/bootstrap",
+    asset: "/assets/winterword/api/asset",
+    image: "/assets/winterword/api/image"
   }
 };
