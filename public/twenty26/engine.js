6c88b13e1a583f2a23f3745f7fd62916e594f23d
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-04-17 07:40:36 +1200

    Update engine.js

--- public/twenty26/engine.js
+++ public/twenty26/engine.js
@@ -1,7 +1,7 @@
 import { WW_CONFIG } from "./config.js";
 
 const REPO_ROOT = "winterword";
-const PUBLIC_ROOT = "assets/winterword";
+const PUBLIC_ROOT = "/winterword/assets/winterword";
 
 const IMAGE_BASE = `${PUBLIC_ROOT}/images`;
 const AUDIO_BASE = `${PUBLIC_ROOT}/audio`;
